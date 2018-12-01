0b274d531a2db532f389fe9a7227939ac452bbd9
Mustafa BOLEKEN <mboleken8@gmail.com>
2018-12-01 21:31:09 +0300

    Update adjustLayer.py

--- adjustLayer.py
+++ adjustLayer.py
@@ -3,13 +3,9 @@
 
 import sys
 import math
-from time import sleep
 import numpy as np
 import pandas as pd
-import pandas_datareader.data as web
-from pandas import Series, DataFrame
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
 from keras.layers import Dense
 from keras.models import Sequential
 import matplotlib.pyplot as plt
@@ -34,14 +30,16 @@ test = data[120:, :]     # length 24
 train2 = data[0:96, :]   # length 96
 test2 = data[96:120, :]     # length 24
 
+
 def prepare_data(data, lags=1):
-    #Create lagged data from an input time series
-    X, y = [], []
+    # Create lagged data from an input time series
+    x, y = [], []
     for row in range(len(data) - lags - 1):
         a = data[row:(row + lags), 0]
-        X.append(a)
+        x.append(a)
         y.append(data[row + lags, 0])
-    return np.array(X), np.array(y)
+    return np.array(x), np.array(y)
+
 
 lags = 1
 
@@ -50,17 +48,18 @@ X_train, y_train = prepare_data(train, lags)
 X_test, y_test = prepare_data(test, lags)
 y_true = y_test
 
-#prepare the data for test on train part
+# prepare the data for test on train part
 X_train2, y_train2 = prepare_data(train2, lags)
 X_test2, y_test2 = prepare_data(test2, lags)
 y_true2 = y_test2
 
+
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setupUI()
+        self.setup_ui()
 
-    def setupUI(self):
+    def setup_ui(self):
         self.setGeometry(600, 200, 1200, 600)
         self.setWindowTitle("Hidden Layer Number Adjuster")
 
@@ -72,7 +71,7 @@ class MyWindow(QWidget):
         self.label_3 = QLabel()
 
         self.pushButton = QPushButton("Calculate")
-        self.pushButton.clicked.connect(self.calculateButton)
+        self.pushButton.clicked.connect(self.calculate_button)
 
         self.progress = QProgressBar(self)
         self.progress.setMaximum(100)
@@ -107,29 +106,26 @@ class MyWindow(QWidget):
 
         self.setLayout(layout)
 
-    def onButtonClick(self):
+    def on_button_click(self):
         self.calc = External()
-        self.calc.countChanged.connect(self.onCountChanged)
+        self.calc.countChanged.connect(self.on_count_changed)
         self.calc.start()
 
-    def onCountChanged(self, value):
+    def on_count_changed(self, value):
         self.progress.setValue(value)
 
-    def neurelNetwork(self, numLayer=1):
-        mdl0=Sequential()
-        mdl0.add(Dense(numLayer, input_dim=lags, activation='relu'))
+    def neurel_network(self, num_layer=1):
+        mdl0 = Sequential()
+        mdl0.add(Dense(num_layer, input_dim=lags, activation='relu'))
         mdl0.add(Dense(1))
         mdl0.compile(loss='mean_squared_error', optimizer='adam')
         return mdl0
 
-    def calculateButton(self):
-<<<<<<< HEAD
+    def calculate_button(self):
         global X_train, y_train, X_test, y_test, y_true, X_split, y_split
         global X_train2, y_train2, X_test2, y_test2, y_true2, X_split2, y_split2
 
-=======
         self.progress.setValue(0)
->>>>>>> 7a63af2dc9e1d256acbadd5f76c01824e2835a49
         x = str(self.lineEdit_0.text())
         y = str(self.lineEdit_1.text())
 
@@ -141,26 +137,27 @@ class MyWindow(QWidget):
             x = 1
             y = 3
 
-        rmseScores = [];
+        rmse_scores = []
 
         mdl1 = []
 
-        for i in range(0,(y-x+1)):
+        for i in range(0, (y-x+1)):
 
-            print("Testing with #",i+x)
-            mdl1.append(self.neurelNetwork(i+x))
+            print("Testing with #", i+x)
+            mdl1.append(self.neurel_network(i+x))
             mdl1[i].fit(X_train2, y_train2, epochs=200, batch_size=2, verbose=0)
             test_predict = mdl1[i].predict(X_test2)
             mse = ((y_test2.reshape(-1, 1) - test_predict.reshape(-1, 1)) ** 2).mean()
-            rmseScores.append(math.sqrt(mse))
+            rmse_scores.append(math.sqrt(mse))
             print("Mean Squared Error: {}".format(mse))
             print("\n")
-            self.progress.setValue((100/(y-x+1))*(i+1)-10)
+            self.progress.setValue(int(100/(y-x+1))*int(i+1)-10)
 
-        bestLayerNumber = rmseScores.index(max(rmseScores))+x
-        self.label_3.setText("\nBest layer number is %d\nIt's RMSE score is %.2f"%(bestLayerNumber, min(rmseScores)))
+        best_layer_number = rmse_scores.index(max(rmse_scores))+x
+        self.label_3.setText("\nBest layer number is %d\nIt's RMSE score is %.2f" %
+                             (best_layer_number, min(rmse_scores)))
 
-        mdl = self.neurelNetwork(bestLayerNumber)
+        mdl = self.neurel_network(best_layer_number)
         mdl.fit(X_train, y_train, epochs=200, batch_size=2, verbose=0)
 
         # generate predictions for training
@@ -186,10 +183,11 @@ class MyWindow(QWidget):
         ax.set_title('Prediction quality: {:.2f} MSE ({:.2f} RMSE)'.format(mse, math.sqrt(mse)))
         ax.plot(y_test.reshape(-1, 1), label='Observed', color='#006699')
         ax.plot(test_predict.reshape(-1, 1), label='Prediction', color='#ff0066')
-        ax.legend(loc='best');
+        ax.legend(loc='best')
         ax.grid()
 
         self.canvas.draw()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
